ddd49a01e3bae0ed4e16a6f43a52962d45d220f3
Tomiatti <27713034+Tomiatti@users.noreply.github.com>
2017-11-21 12:09:25 -0200

    MPI TOPSIS!!!!!!!!!!!!!!!!!!!!!
    leve detalhe um processsador ta fazendo merda

--- topsis_mpi.c
+++ topsis_mpi.c
@@ -1,14 +1,20 @@
+// para compilar: mpicc topsis_mpi.c -o topsis_mpi -lm -Wall
+// para rodar: mpirun -np 2 topsis_mpi
+
+#include <mpi.h>
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 #include<math.h>
 #include<float.h>
 #include<limits.h>
+
 //209554
 #define lin 4
 #define col 4
 
-//Para compilar gcc topsis_serial.c -o topsis_serial -lm
+/*vetor de pesos que todos deverão ter acesso*/
+float VetorPesos[col]  = {1,2,3,4};
 
 //função do qsort para ordenação
 int Ordena(const void * a,const void * b){
@@ -22,27 +28,24 @@ int Ordena(const void * a,const void * b){
 }
 
 void ImprimeVetorMaior(float VetorMaior[lin]){
-   int i;
    printf("\n\n Vetor maior \n\n");
-   for(i=0;i<lin;i++){
+   for(int i=0;i<lin;i++){
 	     printf("[%.3f]\n", VetorMaior[i]);
    }
 }
 
 void ImprimeVetorMenor(float VetorMenor[lin]){
-   int i;
    printf("\n\n Vetor menor \n\n");
-   for(i=0;i<lin;i++){
+   for(int i=0;i<lin;i++){
 	   printf("[%.3f]\n", VetorMenor[i]);
     }
 }
 
 void ImprimeMatrizDados(float MatrizDados[lin][col]){
-   int i,j;
    printf("\n\n Matriz Dados \n\n");
-   for(i=0; i<lin; i++)
+   for(int i=0; i<lin; i++)
     {
-        for(j=0; j<col; j++)
+        for(int j=0; j<col; j++)
         {
              printf("Elemento[%d]= %.3f \n", i,  MatrizDados[i][j]);
         }
@@ -50,10 +53,9 @@ void ImprimeMatrizDados(float MatrizDados[lin][col]){
 }
 
 void ImprimeMatrizNormalizada(float MatrizDadosNormalizada[lin][col]){
-   int i,j;
    printf("\n\n Matriz Normalizada \n\n");
-   for(i=0;i<lin;i++){
-      for(j=0;j<col;j++){
+   for(int i=0;i<lin;i++){
+      for(int j=0;j<col;j++){
 	     printf("[%.3f]", MatrizDadosNormalizada[i][j]);
 	  }
       printf("\n");
@@ -61,7 +63,6 @@ void ImprimeMatrizNormalizada(float MatrizDadosNormalizada[lin][col]){
 }
 
 void ImprimeMatrizSolucaoIdeal(float MatSolucaoIdealPositiva[lin][col]){
-   int i,j;
    printf("\n\n");
 		for(int i=0; i<lin; i++){
 			for(int j=0; j<col; j++){
@@ -69,20 +70,18 @@ void ImprimeMatrizSolucaoIdeal(float MatSolucaoIdealPositiva[lin][col]){
 			}
 			printf("\n");
         }
-}
+    }
 
 void ImprimeVetorSolucaoIdeal(float VetorSolucaoPositiva[col]){
-   int i,j;
     printf("Solucao Positiva\n\n");
 		for(int i=0; i<col; i++){
 			 printf("[%.3f]", VetorSolucaoPositiva[i]);
 		}
         printf("\n");
-}
+    }
 
 void ImprimeMatrizSolucaoNIdeal(float MatSolucaoIdealNegativa[lin][col]){
-   int i,j;
-printf("\n\n");
+printf("Solucao Negativa \n\n");
 		for(int i=0; i<lin; i++){
 			for(int j=0; j<col; j++){
 				printf("[%.3f]", MatSolucaoIdealNegativa[i][j]);
@@ -92,16 +91,14 @@ printf("\n\n");
 }
 
 void ImprimeVetorSolucaoNIdeal(float VetorSolucaoNegativa[col]){
-   int i,j;
 		printf("Solucao Negativa\n\n");
 		for(int i=0; i<col; i++){
 			 printf("[%.3f]", VetorSolucaoNegativa[i]);
 		}
 		printf("\n\n");
-}
-    
+    }
 void ImprimeProximidadeRelativa(float ProximidadeRelativa[col]){    
-    printf("\n");
+    printf("Proximidade Relativa \n\n");
 		for(int i=0; i<col; i++){
 			 printf("Elemento[%d] = %.3f \n", i, ProximidadeRelativa[i]);
 		}
@@ -136,14 +133,13 @@ void NormalizaMatriz(float MatrizDadosNormalizada[lin][col], float MatrizDados[l
     
 }
 
-/*void MultiplicacaoPesos(float MatrizDadosNormalizada[lin][col], float VetorPesos[col]){
-    int i,j;
-    for(i=0; i<lin; i++){
-		for(j=0;j<col;j++){
+void MultiplicacaoPesos(float MatrizDadosNormalizada[lin][col], float VetorPesos[col]){
+    for(int i=0; i<lin; i++){
+		for(int j=0;j<col;j++){
 			MatrizDadosNormalizada[i][j] = MatrizDadosNormalizada[i][j] * VetorPesos[i];
         }
 	}
-}*/
+}
 
 
 
@@ -204,86 +200,141 @@ void SolucaoIdealNegativa(float MatrizDadosNormalizada[lin][col], float VetorMen
 		VetorSolucaoNegativa[i] = sqrtf(VetorSolucaoNegativa[i]); 	
 	} //fim do trecho ________________________________________________________________________________________________________________
 }
-
 void FunProximidadeRelativa(float ProximidadeRelativa[col] , float VetorSolucaoNegativa[col], float VetorSolucaoPositiva[col]){
 
     for(int i = 0; i < col; i++){
-			ProximidadeRelativa[i] = (VetorSolucaoNegativa[i]/(VetorSolucaoPositiva[i] - VetorSolucaoNegativa[i]));
+			ProximidadeRelativa[i] = (VetorSolucaoNegativa[i] / (VetorSolucaoPositiva[i] - VetorSolucaoNegativa[i]) );
     }
 }
-int main (void ){
+
+int main (int argc, char** argv){
+    
+    int world_rank;
+    int world_size;
+    //MPI_Status status;
+    const int root=0;
+
    int i,j;
    //Declaracao da Matriz e vetores
    //float MatrizDados[lin][col];
    float MatrizDadosNormalizada[lin][col];
    float VetorMenor[lin];
    float VetorMaior[lin];
-   float VetorPesos[col]  = {1,2 ,3 ,4};
 
    float MatSolucaoIdealPositiva[lin][col];
    float MatSolucaoIdealNegativa[lin][col];
    float VetorSolucaoPositiva[col] = { };
    float VetorSolucaoNegativa[col] = { };
-   float ProximidadeRelativa[col] = { };
+   float ProximidadeRelativa[col];
 
    float MatrizDados[lin][col] ={{1,2,3,4},
                                 {5,6,7,8},
                                 {9,10,11,12},
                                 {13,14,15,16}};
 
-    NormalizaMatriz(MatrizDadosNormalizada, MatrizDados,VetorPesos);
-    
-	CalculaVetorMaior(VetorMaior, MatrizDadosNormalizada);
-    CalculaVetorMenor(VetorMenor, MatrizDadosNormalizada);
-    
-    //ImprimeMatrizDados(MatrizDados);
-	//ImprimeMatrizNormalizada(MatrizDadosNormalizada);
-	//ImprimeVetorMaior(VetorMaior);
-    //ImprimeVetorMenor(VetorMenor);
-   // ImprimeMatrizNormalizada(MatrizDadosNormalizada);
+    MPI_Init(&argc, &argv);
+        //MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    // Calcula o Vetor de Solucão Positiva Ideal___________________________________________________________________________________
-    for(i=0;i<lin;i++){
-		for(j=0;j<col;j++){
-			MatSolucaoIdealPositiva[i][j] = MatrizDadosNormalizada[i][j] - VetorMaior[i];
-			MatSolucaoIdealPositiva[i][j] = pow(MatSolucaoIdealPositiva[i][j], 2.0);             
-		}		   	
-    }
+        MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+        MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+        MPI_Scatter(MatrizDados, lin*col/world_size, MPI_FLOAT, &MatrizDadosNormalizada, lin*col/world_size, MPI_FLOAT, 0, MPI_COMM_WORLD);
+
+                NormalizaMatriz(MatrizDadosNormalizada, MatrizDados,VetorPesos);        
+            
+            MPI_Barrier(MPI_COMM_WORLD);
         
-	for(i=0; i<lin; i++){ 
+        MPI_Gather(&MatrizDadosNormalizada, lin*col/world_size, MPI_FLOAT, MatrizDados, lin*col/world_size, MPI_FLOAT, 0, MPI_COMM_WORLD);
+
+            if(world_rank == root) {
+                    CalculaVetorMaior(VetorMaior, MatrizDadosNormalizada);
+                    CalculaVetorMenor(VetorMenor, MatrizDadosNormalizada);         
+                }
+          MPI_Bcast(VetorMaior, col, MPI_FLOAT, root, MPI_COMM_WORLD);
+          MPI_Bcast(VetorMenor, col, MPI_FLOAT, root, MPI_COMM_WORLD);    
+              
+       
+        //ImprimeMatrizNormalizada(MatrizDadosNormalizada);
+        //ImprimeVetorMaior(VetorMaior);
+        //ImprimeVetorMenor(VetorMenor);
+        /*tudo certo até aqui*/
+    MPI_Barrier(MPI_COMM_WORLD); 
+    MPI_Scatter(MatrizDadosNormalizada, lin*col/world_size, MPI_FLOAT, &MatSolucaoIdealPositiva, lin*col/world_size, MPI_FLOAT, root, MPI_COMM_WORLD);
+        for(i=0;i<lin;i++){
+            for(j=0;j<col;j++){
+                MatSolucaoIdealPositiva[i][j] = MatrizDadosNormalizada[i][j] - VetorMaior[i];
+                MatSolucaoIdealPositiva[i][j] = pow(MatSolucaoIdealPositiva[i][j], 2.0);                
+            }		   	
+        }
+    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Gather(&MatSolucaoIdealPositiva, lin*col/world_size, MPI_FLOAT, MatrizDadosNormalizada, lin*col/world_size, MPI_FLOAT, root, MPI_COMM_WORLD);
+    MPI_Barrier(MPI_COMM_WORLD);
+   // ImprimeMatrizSolucaoIdeal( MatSolucaoIdealPositiva);
+   //MPI_Scatter(MatSolucaoIdealPositiva, lin*col/world_size, MPI_FLOAT, &VetorSolucaoPositiva, lin, MPI_FLOAT, root, MPI_COMM_WORLD); 
+   
+    //MPI_Barrier(MPI_COMM_WORLD);
+   // MPI_Barrier(MPI_COMM_WORLD);
+  // MPI_Gather(&VetorSolucaoPositiva, lin, MPI_FLOAT, MatSolucaoIdealPositiva, lin, MPI_FLOAT, root, MPI_COMM_WORLD);
+   
+ //  MPI_Barrier(MPI_COMM_WORLD);
+
+   // ImprimeVetorSolucaoIdeal(VetorSolucaoPositiva);       
+    MPI_Scatter(MatrizDadosNormalizada, lin*col/world_size, MPI_FLOAT, &MatSolucaoIdealNegativa, lin*col/world_size, MPI_FLOAT, root, MPI_COMM_WORLD);
+    for(i=0;i<lin;i++){
         for(j=0;j<col;j++){
-            VetorSolucaoPositiva[j] += MatSolucaoIdealPositiva[i][j];
-            VetorSolucaoPositiva[j] = sqrtf(VetorSolucaoPositiva[j]); 
-        }	
-	} //fim do trecho ________________________________________________________________________________________________________________ 
+                MatSolucaoIdealNegativa[i][j] = MatrizDadosNormalizada[i][j] - VetorMenor[i];
+                MatSolucaoIdealNegativa[i][j] = pow(MatSolucaoIdealNegativa[i][j], 2.0);  
+            }		   	
+        }
+    MPI_Barrier(MPI_COMM_WORLD); 
+    MPI_Gather(&MatSolucaoIdealNegativa, lin*col/world_size, MPI_FLOAT, MatrizDadosNormalizada, lin*col/world_size, MPI_FLOAT, root, MPI_COMM_WORLD);
+    MPI_Barrier(MPI_COMM_WORLD);
 
-    ImprimeVetorSolucaoIdeal(VetorSolucaoPositiva);    
-  
-    // Calcula o Vetor de Solucão Negativa Ideal____________________________________________________________________________________
-    for(i=0;i<lin;i++){
-		for(j=0;j<col;j++){
-		    MatSolucaoIdealNegativa[i][j] = MatrizDadosNormalizada[i][j] - VetorMenor[i];
-			MatSolucaoIdealNegativa[i][j] = pow(MatSolucaoIdealNegativa[i][j], 2.0);
-		}		   	
-    }
+    
+  //  MPI_Scatter(MatSolucaoIdealNegativa, lin*col/world_size, MPI_FLOAT, &VetorSolucaoNegativa, lin, MPI_FLOAT, root, MPI_COMM_WORLD);
+    
+   // MPI_Barrier(MPI_COMM_WORLD);
+     
+  //  MPI_Gather(&VetorSolucaoNegativa, lin, MPI_FLOAT, MatSolucaoIdealNegativa, lin*col/world_size, MPI_FLOAT, root, MPI_COMM_WORLD);
+    //ImprimeVetorSolucaoNIdeal(VetorSolucaoNegativa);
+    //MPI_Barrier(MPI_COMM_WORLD);
+       
+         
         
-	for(i=0; i<lin; i++){
-        for(j=0;j<col;j++){ 
-            VetorSolucaoNegativa[j] += MatSolucaoIdealNegativa[i][j];             
-            VetorSolucaoNegativa[j] = sqrtf(VetorSolucaoNegativa[j]);
-        }	
-    } //fim do trecho ________________________________________________________________________________________________________________
     
-    ImprimeVetorSolucaoNIdeal(VetorSolucaoNegativa);    
+        for(i=0; i<lin; i++){ 
+            for(j=0;j<col;j++){           
+                VetorSolucaoPositiva[j] += MatSolucaoIdealPositiva[i][j];
+                VetorSolucaoPositiva[j] = sqrtf(VetorSolucaoPositiva[j]); 
+                
+            }	
+        }   
+        
+        ImprimeVetorSolucaoIdeal(VetorSolucaoPositiva);
+        ImprimeVetorSolucaoNIdeal(VetorSolucaoNegativa);      
+   
+        for(i=0; i<lin; i++){
+            for(j=0;j<col;j++){ 
+                VetorSolucaoNegativa[j] += MatSolucaoIdealNegativa[i][j];
+                VetorSolucaoNegativa[j] = sqrtf(VetorSolucaoNegativa[j]);
+                
+            }	
+        } 
     
+     MPI_Barrier(MPI_COMM_WORLD);
 
-    //Calcula vetor Final de Proximidade Relativa
-	for(int i = 0; i < col; i++){
+
+    for(int i = 0; i < col; i++){
 		ProximidadeRelativa[i] = (VetorSolucaoNegativa[i]/(VetorSolucaoPositiva[i] - VetorSolucaoNegativa[i]));
-	}
-   // FunProximidadeRelativa(ProximidadeRelativa, VetorSolucaoNegativa, VetorSolucaoPositiva);
-	//qsort(ProximidadeRelativa,col,sizeof(float), Ordena);	
-		
+    }
+    
+    MPI_Barrier(MPI_COMM_WORLD);
+      
+    MPI_Finalize();
+      
+    
+    
+    //qsort(ProximidadeRelativa,col,sizeof(float), Ordena);	
 	ImprimeProximidadeRelativa(ProximidadeRelativa);			
 		
     return(0);
